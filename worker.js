d757c08b874a3b1c01b4c875442669661c8d69b0
Filmore9 <66908601+Filmore9@users.noreply.github.com>
2020-06-15 11:15:01 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,56 +1,12 @@
-const STATIC_ASSETS = [ 
-	'./',
-	'./main.js',
-	'./worker.js',
-	'./desktop.css',
-	'./smartphone.css'
-];
+self.onmessage = handleMessage
 
-const STATIC_CACHE_NAME = 'cache-static';
-
-self.addEventListener('install', async evt => {
-	 const cache = await caches.open(STATIC_CACHE_NAME);
-	 cache.addAll(STATIC_ASSETS);
-});
-
-self.addEventListener('fetch', evt => {
-	const req = evt.request;
-	const url = new URL(req.url);
-
-	if (url.origin == location.origin) {
-		evt.respondWith(cacheFirst(req));
-	} else {
-		evt.respondWith(networkFirst(req));
+function handleMessage(e) {
+	console.log(e.data);
+	var hasilAkhir = parseFloat(e.data.uangAwal);
+	for (var i = 0; i < e.data.lamaPenyimpanan; i++) {
+		hasilAkhir += hasilAkhir * (parseFloat(e.data.sbpt) / 100);
 	}
-});
-
-async function cacheFirst(req) {
-	const cachedResponse = await caches.match(req);
-	return cachedResponse || fetch(req);
+	console.log(hasilAkhir);
+	e.data['hasilAkhir'] = hasilAkhir;
+	postMessage(e.data);
 }
-
-async function networkFirst(req) {
-	const cache = await caches.open(STATIC_CACHE_NAME);
-	try {
-        // try go to network and fetch data 
-		const res = await fetch(req);
-		cache.put(req, res.clone());
-		return res;
-	} catch(error) {
-        // look something on cache. 
-		return await cache.match(req);
-	}
-}
-
-self.addEventListener('activate', (evt) => {
-	evt.waitUntil( 
-	    caches.keys().then((keyList) => { 
-	        return Promise.all(keyList.map((key) => { 
-	            if (key !== STATIC_CACHE_NAME) { 
-	                console.log('[ServiceWorker] Removing old cache', key); 
-	                return caches.delete(key); 
-	            } 
-	        }));
-	    })
-	); 
-});
